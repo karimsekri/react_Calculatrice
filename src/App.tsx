aa7420ed295a11cddfffe80bca222a666c4b6b32
karim <karim.sekri@gmail.com>
2023-11-08 16:46:04 +0100

    calcul une fois OK mais je ne peux pas poursuivre le calcul plusieurs fois

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import './App.scss'
 import Result from './components/Result'
 import Chiffre from './components/Chiffre'
 
 function App() {
   const [resultGaucheP, setResultGaucheP] = useState(0)
-  const [resultDroiteP, setResultDroiteP] = useState(0) 
+  const [resultDroiteP, setResultDroiteP] = useState(0)  
+  const [resultFinalP, setResultFinalP] = useState(0) 
   const [symbolP, setSymbolP] = useState("")
+  const [symbolEgaleP, setSymbolEgaleP] = useState("")
+
+
+
 
   const afficherNumber = useCallback((nbreP:number) => {
-    setResultGaucheP(resultGaucheP*10+nbreP)
-  },[resultGaucheP])
+    if (symbolP === ""){
+      setResultGaucheP(resultGaucheP*10+nbreP)      
+    } else if(symbolP != "" && symbolEgaleP === "") {
+      setResultDroiteP(resultDroiteP*10+nbreP)
+    }
+    else if (symbolEgaleP === "=") {
+      setResultFinalP(resultFinalP*10+nbreP)
+      
+    }
+   
+    
+  },[resultGaucheP, resultDroiteP,resultFinalP,  symbolP, symbolEgaleP])
 
   const afficherSigne = useCallback((symbP:string) => {
     setSymbolP(symbP)
   },[symbolP])
 
-  const faireLeCalcul = useMemo(() => {
+  const faireLeCalcul = useCallback(() => {
     if (symbolP === "+") {
-      setResultDroiteP(resultGaucheP+resultDroiteP)
-    } else if (symbolP === "-") {
-      setResultDroiteP(resultGaucheP-resultDroiteP)
-    }
-  },[resultGaucheP, resultDroiteP, symbolP])
+      setSymbolEgaleP(symbolEgaleP+'=')     
+      setResultFinalP(resultGaucheP+resultDroiteP)
+      
+      console.log("symbolEgaleP apres", symbolEgaleP)
+      
 
+
+    } else if (symbolP === "-") {
+      setSymbolEgaleP(symbolEgaleP+'=')
+      setResultFinalP(resultGaucheP-resultDroiteP)
+      
+     
+    }
+  },[resultGaucheP, resultDroiteP, symbolP, symbolEgaleP])
+
+  const effacer = useCallback( () => {
+    setResultFinalP(0)
+    setResultGaucheP(0)
+    setResultDroiteP(0)
+  },[])
 
   return (
     <>
-      <Result value1={resultGaucheP} symbol={symbolP}  value2={resultDroiteP} />
+      <Result value1={resultGaucheP} symbol={symbolP}  value2={resultDroiteP} symbolEgale='' value3={resultFinalP} />
       <div className='chiffre'>
         <Chiffre numberP={0} onClickNumber={afficherNumber} />
         <Chiffre numberP={1} onClickNumber={afficherNumber} />
@@ -43,7 +72,8 @@ function App() {
       <div className='buttonSigne'>
         <button type="button" onClick={() => afficherSigne("-")}>-</button>
         <button type="button" onClick={() => afficherSigne("+")}>+</button>
-        <button type="button" onClick={faireLeCalcul()}>=</button>
+        <button type="button" onClick={() =>faireLeCalcul()}>=</button>
+        <button type="button" onClick={() =>effacer()}>C</button>
       </div>
 
     </>
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,12 +1,35 @@
-const Result = (props : {value1: number, symbol: string, value2 : number  }) => {
+const Result = (props: { value1: number, symbol: string, value2: number, value3: number, symbolEgale: string }) => {
 
+    // console.log ("symbolEgale", props.symbolEgale)
+    console.log ("value1", props.value1)
+    console.log ("value2", props.value2)
+    console.log ("value3", props.value3.toString())
+    console.log ("symbol", props.symbol)
+    
+    let valueFinale = "" ;
 
-    return (
+    if (props.value1 != 0 && props.symbol === "" && props.value2 === 0  && props.symbolEgale === "") {
+       
+        valueFinale = props.value1.toString()
+    }
+    else if (props.value1 != 0 && props.symbol != "" && props.value2 === 0  && props.symbolEgale === "" ) {
+        valueFinale = props.value1.toString() + props.symbol
+    }
+    else if (props.value1 != 0 && props.symbol != "" && props.value2 != 0  && props.value3 === 0) {
+        valueFinale = props.value1.toString() + props.symbol + props.value2.toString() 
+    }else if (props.value3!=0) {
+        valueFinale = props.value3.toString()
+    }
 
-        <div>
-            <input type="text" name="resultInputName" id="resultInputId"  value={props.value1 + ' ' + props.symbol  +' ' + props.value2 } />
-        </div>
-    );
+    console.log ("valueFinale", valueFinale)   
+        return (
+
+            <div>
+                {/* <input type="text" name="resultInputName" id="resultInputId" value={props.value1 + ' ' + props.symbol + ' ' + props.value2 + ' ' + props.symbolEgale + ' ' + props.value3} /> */}
+                <input type="text" name="resultInputName" id="resultInputId" value={valueFinale} />
+            </div>
+        );
+   
 }
 
 export default Result
